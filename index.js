483bca5d45445ad3db6161689489ec96630dace0
putnin.v <putnin.v@gmail.com>
2020-05-13 13:16:09 +0300

    modules 10-12

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log('ssds', __filename);
-const userObj = require('./user');
-console.log(userObj);
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    // res.write('Hello from NodeJS')
+    res.write('<h1>Hello from NodeJS</h1>')
+    res.end(`
+        <div style="background: red; width: 200px; height: 200px;">
+            <h1>Test</h1>
+        </div>
+    `)
+
+})
+
+server.listen(3000, () => {
+    console.log('Server is running...');
+})
--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -2,33 +2,33 @@ const fs = require('fs')
 const path = require('path')
 
 // make dir
-// fs.mkdir(path.join(__dirname, 'notes'), err => {
-//     if (err) throw err
+fs.mkdir(path.join(__dirname, 'notes'), err => {
+    if (err) throw err
 
-//     console.log('Папка была создана')
-// })
+    console.log('Папка была создана')
+})
 
 // write - read
-// fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'), 
-//     'Hello world',
-//     (err) => {
-//         if (err) throw err
-//         console.log('File was created')
+fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'), 
+    'Hello world',
+    (err) => {
+        if (err) throw err
+        console.log('File was created')
 
-//         fs.appendFile(path.join(__dirname, 'notes', 'mynotes.txt'),
-//          ' From append file',
-//         err => {
-//             if (err) throw err
-//             console.log('File was changed')
+        fs.appendFile(path.join(__dirname, 'notes', 'mynotes.txt'),
+         ' From append file',
+        err => {
+            if (err) throw err
+            console.log('File was changed')
 
-//             fs.readFile(path.join(__dirname, 'notes', 'mynotes.txt'), 
-//                 'utf-8',
-//                 (err, data) => {
-//                    if (err) throw err
-//                    console.log(data)
-//                 })
-//         })
-//     })
+            fs.readFile(path.join(__dirname, 'notes', 'mynotes.txt'), 
+                'utf-8',
+                (err, data) => {
+                   if (err) throw err
+                   console.log(data)
+                })
+        })
+    })
 
 fs.rename(
     path.join(__dirname, 'notes', 'mynotes.txt'),
