8353af368adbba7bd09e15d18fc978a8b32e947d
putnin.v <putnin.v@gmail.com>
2020-05-16 00:40:52 +0300

    modules 22-26

--- NODE-EXPRESS/index.js
+++ NODE-EXPRESS/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 const app = express()
 
 const hbs = exphbs.create({
@@ -12,14 +15,11 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-app.get('/', (req, res) => {
-    res.status(200)
-    res.render('index')
-})
-
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
 const PORT = process.env.PORT || 3000
 
