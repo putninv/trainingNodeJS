1a7608e9013020627c61187254f643d7ee1bc9df
putnin.v <putnin.v@gmail.com>
2020-05-30 21:43:35 +0300

    modules 41-43

--- NODE-EXPRESS/index.js
+++ NODE-EXPRESS/index.js
@@ -8,12 +8,8 @@ const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const cardRoutes = require('./routes/card')
+const User = require('./models/user')
 const app = express()
-
-const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-})
 
 app.engine('hbs', exphbs({
     handlebars: allowInsecurePrototypeAccess(Handlebars),
@@ -21,10 +17,18 @@ app.engine('hbs', exphbs({
     extname: 'hbs'
 }));
 app.set('view engine', 'hbs');
-
-// app.engine('hbs', hbs.engine)
-// app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('5ed2a28692cefb2b14511be4')
+        req.user = user
+        next()
+    } catch(e) {
+        console.log(e)
+    }
+    
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
@@ -37,13 +41,22 @@ const PORT = process.env.PORT || 3000
 
 async function start() {
     try {
-        const password = '284516Mnb'
         const dbUrl = `mongodb://putninv:284516Mnb@cluster0-shard-00-00-xdylf.mongodb.net:27017,cluster0-shard-00-01-xdylf.mongodb.net:27017,cluster0-shard-00-02-xdylf.mongodb.net:27017/shop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
         Cop`
-        await mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+        await mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
+        const candidate =await User.findOne()
+
+        if(!candidate) {
+            const user = new User({
+                email: 'def_user@gmail.com',
+                name: 'Piter',
+                cart: {items: []}
+            })
+            await user.save()
+        }
     } catch(e) {
         console.log(e)
     }
--- NODE-EXPRESS/routes/courses.js
+++ NODE-EXPRESS/routes/courses.js
@@ -3,7 +3,7 @@ const Course = require('../models/course')
 const router = Router()
 
 router.get('/',async (req, res) => {
-    const courses = await Course.find()
+    const courses = await Course.find().populate('userId', 'email name')
     res.render('courses',{
         title: 'Courses',
         isCourses: true,
@@ -29,6 +29,15 @@ router.post('/edit', async (req, res) => {
     delete req.body.id
     await Course.findByIdAndUpdate(id, req.body)
     res.redirect('/courses')
+})
+
+router.post('/remove', async (req, res) => {
+    try{
+        await Course.deleteOne({_id: req.body.id})
+        res.redirect('/courses')
+    } catch(e) {
+        console.log(e)
+    }
 })
 
 router.get('/:id', async (req, res) => {
